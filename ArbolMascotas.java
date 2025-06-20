4ff288b87b45827812d4e20e5522a68eb3aee128
unknown <abdias.hidalgo@ucr.ac.cr>
2025-06-20 17:34:26 -0600

    Arbol para registro de veterinaria, falta asegurar que el id es unico en la clase mascota

---
+++ ArbolMascotas.java
@@ -0,0 +1,127 @@
+public class ArbolMascotas{
+
+    private NodoABB raiz;
+
+    public ArbolMascotas(Mascota mascota){
+        this.raiz = new NodoABB( mascota);
+    }
+    
+    public void agregarMascota(Mascota mascota){
+        
+        raiz = agregarRecursivo(this.raiz, mascota);
+    }
+
+    private NodoABB agregarRecursivo(NodoABB actual, Mascota mascota){
+
+        if(actual == null){
+            return new NodoABB(mascota);
+        }
+        if( actual.getMascota().getId() < mascota.getId()){
+
+            actual.setDerecha(agregarRecursivo(actual.getDerecha(), mascota));
+
+        }
+        if( actual.getMascota().getId() < mascota.getId()){
+
+            actual.setIzquierda(agregarRecursivo(actual.getIzquierda(), mascota));
+
+        }
+        return actual;
+
+    }
+
+    public boolean eliminarMascota(int identificador){
+
+        NodoABB nodoEliminar = buscarRecursivo(this.raiz, identificador);
+        if (nodoEliminar == null) return false;
+        
+        raiz = eliminarRecursivo(identificador, this.raiz);
+        return true;
+
+    }
+    private NodoABB eliminarRecursivo(int identificador, NodoABB nodo){
+
+        if(nodo == null){
+            return null;
+        }
+        if(nodo.getMascota().getId() > identificador) {
+            nodo.setIzquierda(eliminarRecursivo(identificador, nodo.getIzquierda()));
+        }
+        if(nodo.getMascota().getId() < identificador) {
+            nodo.setDerecha(eliminarRecursivo(identificador, nodo.getDerecha()));
+        }
+
+        if(nodo.getMascota().getId() == identificador){
+            //caso sin hijos
+            if(nodo.getDerecha() == null && nodo.getIzquierda() == null){
+                return null;
+
+            }
+            // caso un hijo
+            if(nodo.getDerecha() == null){
+                return nodo.getIzquierda();
+            }
+            if(nodo.getIzquierda() == null){
+                return nodo.getDerecha();
+            }
+
+            //caso 2hijos
+            Mascota mascotaMin = encontrarMinimo(nodo.getDerecha());
+            nodo.setMascota(mascotaMin);
+            nodo.setDerecha(eliminarRecursivo(mascotaMin.getId(), nodo.getDerecha()));
+            return nodo;
+        }
+        return nodo;
+    }
+    private Mascota encontrarMinimo(NodoABB nodo){
+
+        if (nodo.getIzquierda() == null) {
+            return nodo.getMascota();
+        } else {        
+            return encontrarMinimo(nodo.getIzquierda());
+        }
+    }
+
+    public Mascota buscarMascota(int identificador){
+
+        raiz = buscarRecursivo(this.raiz, identificador);
+        if(raiz == null) {
+            return null;
+        } else{
+        return raiz.getMascota();
+        }
+
+    }
+    private NodoABB buscarRecursivo(NodoABB nodo, int identificador){
+
+        if(nodo.getMascota().getId() == identificador) return nodo;
+
+        if(nodo == null) return null;
+
+        if(nodo.getMascota().getId() > identificador){
+            nodo.setIzquierda(buscarRecursivo(nodo.getIzquierda(), identificador));
+        } else{
+            nodo.setDerecha(buscarRecursivo(nodo.getDerecha(), identificador));
+        }
+        return nodo;
+
+    }
+    public void recorridoEnOrden(){
+
+        recorridoEnOrdenRecursivo(raiz);
+
+    }
+    public void recorridoEnOrdenRecursivo(NodoABB nodo) {
+
+        if (nodo != null && nodo.getMascota() != null) {
+            NodoABB nodoIzquierdo = nodo.getIzquierda();
+            NodoABB nodoDerecho = nodo.getDerecha();
+        
+            recorridoEnOrdenRecursivo(nodoIzquierdo);
+            System.out.println("ID: " + nodo.getMascota().getId() + 
+                            ", Tipo: " + nodo.getMascota().getTipo());
+            recorridoEnOrdenRecursivo(nodoDerecho);
+        }
+    }
+
+}
