580c37301208414b7868c64544b50cdcc1aa6fdb
unknown <abdias.hidalgo@ucr.ac.cr>
2025-06-22 22:15:34 -0600

    agregado generar id unico

--- Mascota.java
+++ Mascota.java
@@ -1,19 +1,28 @@
 public class Mascota{
 
+    private static final int RANGO_MAX = 10000; 
     private int id;
     private String tipo;
 
-    public Mascota(String tipo, int id){
-        this.id = id;
+    public Mascota(String tipo){
+        this.id = 0; 
         this.tipo = tipo;
+        this.setId();
     }
 
     public String getTipo() {
-    return this.tipo;
+        return this.tipo;
     }
-    public void setId(int id){
-        this.id = id;
+
+    public void setId() {
+        long nuevoId;
+        do {
+            nuevoId = (long) (Math.random() * RANGO_MAX);
+        } while (nuevoId == this.id);
+    
+        this.id = (int) nuevoId;
     }
+
     public int getId(){
         return this.id;
     }
